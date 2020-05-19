8c44e7c01e65ed2a737965f02d747737228b71fe
Candice Marie Martinez <39352383+CMarieMartinez@users.noreply.github.com>
2020-05-18 20:27:51 -0400

    Update purchases-confirmation-page.js

--- purchases-confirmation-page.js
+++ purchases-confirmation-page.js
@@ -1,13 +1,13 @@
-dataLayer = [{'pageType' : 'confirmationPage'}];
+dataLayer = [{'pageType' : 'confirmationPage'}]; // Goes into the <head></head> above GA snippet
 
  function getOrder() {
         var layout_class = document.getElementsByClassName("layout layout-1-col")[0];
         var getDiv = layout_class.getElementsByTagName("div")[0];
         var findA = getDiv.getElementsByTagName("p")[0];
-        var transactionId = findA.getElementsByTagName("a")[0].innerHTML;
+        var transactionId = findA.getElementsByTagName("a")[0].innerHTML;  // Pull transaction id
 
-        var orderObj = localStorage.getItem("ecomm");
+        var orderObj = localStorage.getItem("ecomm"); // Call incomplete ecommerce object from local storage
         var ecommerce = JSON.parse(orderObj);
-        ecommerce.purchase.actionField.id = transactionId;
-        dataLayer.push({ 'ecommerce': ecommerce });
+        ecommerce.purchase.actionField.id = transactionId; // Add transaction id to complete ecommerce object
+        dataLayer.push({ 'ecommerce': ecommerce }); // Push ecommerce object to complete conversion funnel
     } 
